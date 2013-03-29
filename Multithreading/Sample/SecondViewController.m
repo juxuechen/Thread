8f68a8b1dee51fe9f2dcd7c260051a79896a1f11
琚雪 <juxue.cy@taobao.com>
2013-03-29 14:46:46 +0800

    例子2：operation

--- Multithreading/Sample/SecondViewController.m
+++ Multithreading/Sample/SecondViewController.m
@@ -35,7 +35,6 @@
     
     serialNumber = 1;
     
-    //堵塞主线程了，UI无法及时。。。。
     NSOperationQueue *queue = [[NSOperationQueue alloc] init];
     [queue setMaxConcurrentOperationCount:5];
     for (int j = 1; j <= 20; j++) {
@@ -46,18 +45,27 @@
 }
 
 - (void)addView:(NSNumber *)num{
-    @synchronized(self){
+    @synchronized(self){//只有一个线程可以访问这个函数
         NSLog(@"第%d个进来",[num intValue]);
         int j = [num intValue];
-        UILabel *label= [[UILabel alloc] initWithFrame:CGRectMake(60, 12 + 15 * serialNumber, 200, 12)];
-        label.backgroundColor = [UIColor greenColor];
-        label.text = [NSString stringWithFormat:@"%d是第%d个添加上界面",j,serialNumber];
-        label.font = [UIFont systemFontOfSize:12.];
-        serialNumber++;
-        
-        [self.view performSelectorOnMainThread:@selector(addSubview:) withObject:label waitUntilDone:NO];
+
+        [self performSelectorOnMainThread:@selector(updateUI:)
+                               withObject:[NSNumber numberWithInt:j]
+                            waitUntilDone:NO];
         //waitUntilDone是表示是否阻塞子线程
     }
+}
+
+- (void)updateUI:(NSNumber *)number {
+    UILabel *label= [[UILabel alloc] initWithFrame:CGRectMake(60, 12 + 15 * serialNumber, 200, 12)];
+    label.backgroundColor = [UIColor greenColor];
+    label.font = [UIFont systemFontOfSize:12.];
+    label.text = [NSString stringWithFormat:@"%d是第%d个添加上界面",[number intValue],serialNumber];
+    [self.view addSubview:label];
+    
+    serialNumber++;
+    
+    NSLog(@"updateui %@",label);
 }
 
 - (void)didReceiveMemoryWarning
--- Multithreading/Sample/TestOperation.m
+++ Multithreading/Sample/TestOperation.m
@@ -11,27 +11,23 @@
 
 @implementation TestOperation
 
-
-- (BOOL)isConcurrent {//异步方式
+//是否允许并发 默认no
+- (BOOL)isConcurrent {
     return YES;
 }
 
+//执行任务主函数，线程运行的入口函数
 - (void)start {
     if (![self isCancelled]) {
         //很多任务同时执行。
         NSLog(@"start 第%d个任务",self.i);
         
-        
-        for(int bb = 0; bb < 10000; bb++)
-            for(int aa = 0; aa < 10000; aa++);
-        
-         NSLog(@"start 第%d个任务  --- 运算完毕",self.i);
+        for(int b = 0; b < 5000; b++)
+            for(int aa = 0; aa < 50000; aa++);
     }
     
     [super start];
 }
-
-
 
 
 @end
